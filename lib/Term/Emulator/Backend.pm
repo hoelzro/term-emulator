9c239a720cff16b494aad28e7a25a064fcb0bbde
Rob Hoelz <rob@hoelz.ro>
2013-08-09 22:23:38 +0200

    Add skeletons for backend role and Cairo backend

---
+++ lib/Term/Emulator/Backend.pm
@@ -0,0 +1,19 @@
+## no critic (RequireUseStrict)
+package Term::Emulator::Backend;
+
+## use critic (RequireUseStrict)
+use Moose::Role;
+
+1;
+
+__END__
+
+# ABSTRACT: Backend objects for L<Term::Emulator>
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
---
+++ lib/Term/Emulator/Backend/Cairo.pm
@@ -0,0 +1,25 @@
+## no critic (RequireUseStrict)
+package Term::Emulator::Backend::Cairo;
+
+## use critic (RequireUseStrict)
+use Moo;
+
+with 'Term::Emulator::Backend';
+
+1;
+
+__END__
+
+# ABSTRACT: Cairo backend for Term::Emulator
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 SEE ALSO
+
+L<Term::Emulator::Backend>
+
+=cut
